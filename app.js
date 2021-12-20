142fbdae69fcd754e07fe3b929be0abd280d3c53
Mohammed Mazahim <mazamim91@gmail.com>
2021-12-21 10:06:25 +1100

    updated2

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 //display today date
-var todayDate = moment()
-var todayDate = moment().format("dddd, MMMM Do")
+
+//  var todayDate = moment().format("dddd, MMMM Do")
+ var todayDate = moment().format("dddd, MMMM Do YYYY,h:mm:ss a")
 $("#todayDate").text(todayDate);
+
+function displayTime(){
+    var todayDate = moment().format("dddd, MMMM Do YYYY,h:mm:ss a")
+    $("#todayDate").text(todayDate);
+}
+setInterval(displayTime, 1000);
+
+// date from local storage
+// var localData=[
+//     {
+//         DDate:'2021-12-21T09:00:00+11:00',
+//         description:'Need to sleep alot i feel sleepy9',
+//         status:'completed'
+//     },
+//     {
+//         DDate:'2021-12-21T10:00:00+11:00',
+//         description:'Need to sleep alot i feel sleepy10',
+//         status:'pending'
+//     }
+
+// ]
+//get current date and time
+
+//set class dependon the time
+
+
+var localData=[]
+
 
  var data=generateData(9,17)
 
@@ -10,15 +39,26 @@ $("#todayDate").text(todayDate);
 function generateData (start,end){
     const items = []
     for (let hour = start; hour < end+1; hour++) {
-      let time=moment({ hour }).format('h:mm A')
-      let description=""
+        let description=""
+       var timetoDisplay=moment({ hour }).format("h:mm:ss a")
+     var timeforCalculation=moment({ hour }).format()
+
+localData.forEach((element)=>{
+
+        if(element.DDate=== timeforCalculation){
+            description=element.description
+      
+        }
+})
+
+    
         items.push({
-          time:time,
+          time:timeforCalculation,
           description:description,
           status:'pending'
       })
 
-      createElements(time,description);
+      createElements(timetoDisplay,description,timeforCalculation);
      // items.push(moment({ hour, minute: 60 }).format('h:mm A'))
     }
     return items
@@ -26,34 +66,55 @@ function generateData (start,end){
 
 
 //   create Elements
-function createElements(time,description){
+function createElements(timetoDisplay,description,timeforCalculation){
     parentContainer=document.getElementById("parentContainer")
     const parentDiv = document.createElement("div");
         parentDiv.classList.add('row')
     const div1 = document.createElement("div");
                 div1.classList.add("col","hour")
         const timeP=document.createElement('p')
-        timeP.innerHTML=time
+        timeP.innerHTML=timetoDisplay
     const div2 = document.createElement("div");
         const txtArea=document.createElement("textarea");
         txtArea.classList.add("textarea")
+        txtArea.textContent=description
    const div3 = document.createElement("div");
         const saveP = document.createElement('p')
         const ico = document.createElement('i')
         ico.classList.add("fa" ,"fa-floppy-o")
         ico.setAttribute("style", "font-size: 30px; padding-top:10px");
    
+
   
-  
-      
-    div2.classList.add("col-8","past")
+      // get current time and check 
+
+            x=moment(timeforCalculation)
+            y=moment()
+            var duration = moment.duration(x.diff(y))
+        var durationInmin = (duration._data.hours*60)+duration._data.minutes
+      if (durationInmin<=0 && durationInmin>-60){
+        div2.classList.add("col-8","present")
+   
+      }
+      if (durationInmin<=-60){
+        div2.classList.add("col-8","past")
+
+      }
+
+      if (durationInmin>0){
+        div2.classList.add("col-8","future")
+    
+      }
+
+
+     
         
     div3.classList.add("col-xs-12", "col-sm-1", "saveBtn")
 
     parentContainer.appendChild(parentDiv);
     parentDiv.appendChild(div1)
         div1.appendChild(timeP)
-        timeP.textContent=time
+        timeP.textContent=timetoDisplay
     parentDiv.appendChild(div2)
         div2.appendChild(txtArea)
     parentDiv.appendChild(div3)
